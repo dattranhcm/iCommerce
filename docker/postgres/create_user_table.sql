bdc434b3ac4b2779f2cb355f8acf79dcc57f7c66
dat <dat.tran@ht-tech.vn>
2020-12-10 18:04:23 +0700

    add sql init user table and data file

---
+++ docker/postgres/create_user_table.sql
@@ -0,0 +1,13 @@
+\c dbuser;
+DROP TABLE IF EXISTS t_users CASCADE;
+create table t_users (
+   id SERIAL PRIMARY KEY NOT NULL,
+   first_name TEXT NOT NULL,
+   last_name TEXT NOT NULL,
+   email TEXT NOT NULL,
+   mobile TEXT NOT NULL,
+   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
+
+INSERT INTO t_users (first_name,last_name,email,mobile) values ('Dat', 'Tran', 'hoangdat@gmail.com', '0393974369');
